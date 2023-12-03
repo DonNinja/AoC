90ff325f111f9adfe2bdbf00c016d1c93c735c3f
DonNinja <yngvileo@gmail.com>
2023-12-03 12:48:10 +0000

    Part 2 is done

--- AoC 2023/Day3/Program.cs
+++ AoC 2023/Day3/Program.cs
@@ -10,23 +10,28 @@ namespace Day3 {
             Regex numRegex = new(@"0-9");
             bool foundNumber = false;
             int result = 0;
+            int gearRatioResult = 0;
 
             for (int i = 0; i < data.Length; i++) {
                 foundNumber = false;
                 string line = data[i];
 
                 for (int j = 0; j < line.Length; j++) {
+                    string letter = line[j].ToString();
+
+                    if (letter == "*")
+                        gearRatioResult += GetGearRatios(data, [i, j]);
 
                     if (!foundNumber && LookAround(data, [i, j])) {
-                        if (!new Regex(@"\d").IsMatch(line[j].ToString()))
+                        if (!new Regex(@"\d").IsMatch(letter))
                             continue;
 
                         foundNumber = true;
 
                         int gn = GetNumber(line, j);
-                        //if (gn == 664)
-                        //    Console.WriteLine("");
+
                         //Console.WriteLine(string.Format("Found a part number: {0}", gn));
+
                         result += gn;
                     }
                     else if (foundNumber && symbolRegex.IsMatch(line[j].ToString())) {
@@ -36,6 +41,7 @@ namespace Day3 {
             }
 
             Console.WriteLine(string.Format("The result should be: {0}", result));
+            Console.WriteLine(string.Format("The gear ratio result should be: {0}", gearRatioResult));
         }
 
         /// <summary>
@@ -64,8 +70,45 @@ namespace Day3 {
             return false;
         }
 
+        static int GetGearRatios(string[] data, int[] position) {
+            List<int> gears = new();
+            Regex numRegex = new Regex(@"\d");
+
+            for (int i = -1; i <= 1; i++) {
+                bool foundNumber = false;
+                for (int j = -1; j <= 1; j++) {
+                    try {
+                        int line = position[0] + i;
+                        int index = position[1] + j;
+                        // This is fucking stupid
+                        string letter = data[line][index].ToString();
+
+                        if (!foundNumber && numRegex.IsMatch(letter)) {
+                            int num = GetNumber(data[line], index);
+                            foundNumber = true;
+                            if (!gears.Contains(num)) {
+                                gears.Add(num);
+                            }
+                        }
+                        else {
+                            foundNumber = false;
+                        }
+                    }
+                    catch (IndexOutOfRangeException) {
+                        // If it's out of range, it doesn't matter
+                    }
+                }
+            }
+
+            if (gears.Count != 2) {
+                return 0;
+            }
+
+            return gears[0] * gears[1];
+        }
+
         /// <summary>
-        /// This gets the number 
+        /// This gets the number
         /// </summary>
         /// <param name="str"></param>
         /// <param name="index"></param>
