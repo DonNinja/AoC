9a19600e3600a6c6c56818dc07e93d3caaf5d55a
Yngvilth <yngvilth@gmail.com>
2024-12-07 13:14:04 +0000

    Split lines into test value and values to calculate

--- AoC-2024/Day-7/main.py
+++ AoC-2024/Day-7/main.py
@@ -1,6 +1,21 @@
 data = open("AoC-2024/Day-7/testinput.txt", "r")
 
+operators = ["*", "+"]
+
+def evaluate(values, operator):
+    if operator == "*":
+        return values[0] * values[1]
+    elif operator == "+":
+        return values[0] + values[1]
+    
+    return 0
+    pass
+
 for line in data:
+    sLine = line.strip()
+    
+    testValue, calcValues = sLine.split(":")
+    intValues = [int(x) for x in calcValues.strip().split(" ")]
     
     pass
 
