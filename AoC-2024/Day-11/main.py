36825326b0f0ce740b1b94ee584ef49333325cea
Yngvilth <yngvilth@gmail.com>
2024-12-11 20:08:49 +0000

    Day 1 finished

--- AoC-2024/Day-11/main.py
+++ AoC-2024/Day-11/main.py
@@ -1,24 +1,37 @@
-data = open("AoC-2024/Day-11/smalltestinput", "r")
+data = open("AoC-2024/Day-11/input", "r")
 
 
 def evolve(stones: list):
     newStones = []
 
     for stone in stones:
-        if len(stone) % 2 == 0:
+        if stone == "0":
+            newStones.append("1")
+
+        elif len(stone) % 2 == 0:
+            halfLength = int(len(stone) / 2)
+            newStones.append(str(int(stone[:halfLength])))
+            newStones.append(str(int(stone[halfLength:])))
             pass
-        
-        pass
+
+        else:
+            newStones.append(str(int(stone) * 2024))
 
     return newStones
-    pass
 
 
 initial = data.readline().strip()
 
+stoneList = initial.split(" ")
 
-for i in range(1):
+for i in range(25):
+    stoneList = evolve(stoneList)
 
-    pass
+    # for stone in stoneList:
+    #     print(stone, end=' ')
+    # print()
+    
+print(len(stoneList))
+
 
 data.close()
