6a5615cf8ced57b3d55bf286e90979eded191651
Yngvilth <yngvilth@gmail.com>
2024-12-11 19:31:01 +0000

    Started day 11

---
+++ AoC-2024/Day-11/main.py
@@ -0,0 +1,24 @@
+data = open("AoC-2024/Day-11/smalltestinput", "r")
+
+
+def evolve(stones: list):
+    newStones = []
+
+    for stone in stones:
+        if len(stone) % 2 == 0:
+            pass
+        
+        pass
+
+    return newStones
+    pass
+
+
+initial = data.readline().strip()
+
+
+for i in range(1):
+
+    pass
+
+data.close()
