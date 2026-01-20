d59227752d1fa77dc412d46e6e03d347a1388d10
Yngvilth <yngvilth@gmail.com>
2026-01-20 07:28:06 +0000

    Got part way into part 2

--- AoC-2025/Day-4/main.py
+++ AoC-2025/Day-4/main.py
@@ -1,43 +1,53 @@
-data = open("AoC-2025\\Day-4\\input.txt", "r")
+data = open("AoC-2025\\Day-4\\testinput.txt", "r")
 
 results = 0
 
 dataArray = data.readlines()
+paperToRemove = 1
 
-for l in range(len(dataArray)):
-    line = dataArray[l].strip()
-    # print(line)
-    for i in range(len(line)):
-        if line[i] == "@":
-            tooMany = False
-            paperCount = 0
-            for y in range(-1, 2):
-                for x in range(-1, 2):
-                    # Don't count current position
-                    if y == 0 and x == 0:
-                        continue
+while paperToRemove > 0:
+    paperToRemove = 0
 
-                    # Check if we won't go outside of the list
-                    inXRange = i + x >= 0 and i + x < len(line)
-                    inYRange = l + y >= 0 and l + y < len(dataArray)
+    for l in range(len(dataArray)):
+        line = dataArray[l].strip()
+        # print(line)
+        removalLocations = []
+        for i in range(len(line)):
+            if line[i] == "@":
+                tooMany = False
+                paperCount = 0
+                for y in range(-1, 2):
+                    for x in range(-1, 2):
+                        # Don't count current position
+                        if y == 0 and x == 0:
+                            continue
 
-                    if inYRange and inXRange:
-                        if dataArray[l + y][i + x] == "@":
-                            paperCount += 1
-                            if paperCount > 3:
-                                tooMany = True
-                                break
+                        xLoc = i + x
+                        yLoc = l + y
+                        # Check if we won't go outside of the list
+                        inXRange = xLoc >= 0 and xLoc < len(line)
+                        inYRange = yLoc >= 0 and yLoc < len(dataArray)
 
-                if tooMany:
-                    break
+                        if inYRange and inXRange:
+                            if dataArray[yLoc][xLoc] == "@":
+                                paperCount += 1
+                                if paperCount > 3:
+                                    tooMany = True
+                                    break
 
-            if not tooMany:
-                results += 1
-                    # print(dataArray[l + y][i + x], end="")
-                # print()
-            # print(line[i], end="")
+                    if tooMany:
+                        break
+
+                if not tooMany:
+                    # paperToRemove += 1
+                    removalLocations.append((yLoc, xLoc))
+                    results += 1
+                        # print(dataArray[l + y][i + x], end="")
+                    # print()
+                # print(line[i], end="")
+            # print()
+
         # print()
 
-    # print()
 
 print(results)
