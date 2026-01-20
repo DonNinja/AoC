8ee6456a11717af1646a960a8a1e1cfcc1d09cea
Yngvilth <yngvilth@gmail.com>
2026-01-20 07:19:16 +0000

    Day 4 part 1 finished

--- AoC-2025/Day-4/main.py
+++ AoC-2025/Day-4/main.py
@@ -1,16 +1,43 @@
-data = open("AoC-2025\\Day-4\\testinput.txt", "r")
+data = open("AoC-2025\\Day-4\\input.txt", "r")
 
 results = 0
-lineNum = 0
 
-for line in data:
-    line = line.strip()
+dataArray = data.readlines()
+
+for l in range(len(dataArray)):
+    line = dataArray[l].strip()
     # print(line)
     for i in range(len(line)):
-        print(line[i], end="")
-    
-    print()
-    
-    lineNum += 1
+        if line[i] == "@":
+            tooMany = False
+            paperCount = 0
+            for y in range(-1, 2):
+                for x in range(-1, 2):
+                    # Don't count current position
+                    if y == 0 and x == 0:
+                        continue
 
-# print(results)
+                    # Check if we won't go outside of the list
+                    inXRange = i + x >= 0 and i + x < len(line)
+                    inYRange = l + y >= 0 and l + y < len(dataArray)
+
+                    if inYRange and inXRange:
+                        if dataArray[l + y][i + x] == "@":
+                            paperCount += 1
+                            if paperCount > 3:
+                                tooMany = True
+                                break
+
+                if tooMany:
+                    break
+
+            if not tooMany:
+                results += 1
+                    # print(dataArray[l + y][i + x], end="")
+                # print()
+            # print(line[i], end="")
+        # print()
+
+    # print()
+
+print(results)
