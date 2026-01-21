fb224a3bf994da167c165783447f84a6aeac23cd
Yngvilth <yngvilth@gmail.com>
2026-01-21 18:07:43 +0000

    This doesn't give the correct answer
    Potentially just calculating incorrectly :/

--- AoC-2025/Day-5/main.py
+++ AoC-2025/Day-5/main.py
@@ -25,21 +25,53 @@ for line in data:
 
     if freshCheck:
         if rangeFinding:
-            minRange, maxRange = line.split("-")
-            freshRanges.append((int(minRange), int(maxRange)))
+            minID, maxID = line.split("-")
+            freshRanges.append((int(minID), int(maxID)))
 
         else:
             currID = int(line)
-            
-            for range in freshRanges:
-                if currID >= range[0] and currID <= range[1]:
-                    # print("Ingredient with ID {0} is fresh.".format(line))
+
+            for fRange in freshRanges:
+                if currID >= fRange[0] and currID <= fRange[1]:
                     results += 1
                     break
     else:
-        minRange, maxRange = line.split("-")
-        
-        pass
+        minID, maxID = line.split("-")
+        minID = int(minID)
+        maxID = int(maxID)
 
+        minRangeIndex, maxRangeIndex = -1, -1
+
+        for i in range(len(freshRanges)):
+            fRange = freshRanges[i]
+
+            if fRange[1] >= minID and fRange[0] <= minID:
+                minRangeIndex = i
+
+            if fRange[0] <= maxID and fRange[1] >= maxID:
+                maxRangeIndex = i
+
+        if minRangeIndex != maxRangeIndex:
+            minRange = freshRanges[minRangeIndex]
+            maxRange = freshRanges[maxRangeIndex]
+
+            if maxRangeIndex == -1:
+                freshRanges[minRangeIndex] = (minRange[0], maxID)
+            elif minRangeIndex == -1:
+                freshRanges[maxRangeIndex] = (minID, maxRange[1])
+            else:
+                freshRanges[minRangeIndex] = (minRange[0], maxRange[1])
+                freshRanges.pop(maxRangeIndex)
+        elif minRangeIndex == -1 and maxRangeIndex == -1:
+            freshRanges.append((minID, maxID))
+            # pass
+        pass
+    
+freshRanges.sort()
+
+if not freshCheck:
+    for r in freshRanges:
+        results += r[1] - r[0]
+        # results += 1
 
 print(results)
