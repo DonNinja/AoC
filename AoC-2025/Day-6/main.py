1de37074c3f7626a9ed26573e9d0169c3a895b43
Yngvilth <yngvilth@gmail.com>
2026-01-22 07:14:52 +0000

    Day 6 part 1 finished

--- AoC-2025/Day-6/main.py
+++ AoC-2025/Day-6/main.py
@@ -1,4 +1,4 @@
-file = open("AoC-2025\\Day-6\\testinput.txt", "r")
+file = open("AoC-2025\\Day-6\\input.txt", "r")
 
 results = 0
 
@@ -9,6 +9,25 @@ file.close()
 calcArray = []
 
 for line in data:
-    for entry in line.split():
-        pass
-    pass
+    entries = line.strip().split()
+    if len(calcArray) == 0:
+        for _ in range(len(entries)):
+            calcArray.append([])
+    for i in range(len(entries)):
+        calcArray[i].append(entries[i])
+
+
+for calc in calcArray:
+    total = 0
+    operator = calc[-1]
+    for c in range(len(calc) - 1):
+        if total == 0:
+            total = int(calc[c])
+        else:
+            if operator == "+":
+                total += int(calc[c])
+            elif operator == "*":
+                total *= int(calc[c])
+    results += total
+
+print(results)
