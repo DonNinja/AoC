9016c78d55be84e010855288b022a76854e01501
Yngvilth <yngvilth@gmail.com>
2026-01-22 06:55:20 +0000

    Start day 6

---
+++ AoC-2025/Day-6/main.py
@@ -0,0 +1,14 @@
+file = open("AoC-2025\\Day-6\\testinput.txt", "r")
+
+results = 0
+
+data = file.readlines()
+
+file.close()
+
+calcArray = []
+
+for line in data:
+    for entry in line.split():
+        pass
+    pass
