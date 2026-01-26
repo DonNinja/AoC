62e04fbcdf4bf8d18787c0197502b51027584f9b
Yngvilth <yngvilth@gmail.com>
2026-01-26 16:48:36 +0000

    Update main.py

--- AoC-2025/Day-9/main.py
+++ AoC-2025/Day-9/main.py
@@ -1,3 +1,14 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.xPoint = None
+        self.yPoint = None
+    
+    def isInLoop(self, other) -> bool:
+        return False
+    
+
 file = open("AoC-2025\\Day-9\\input.txt", "r")
 
 results: int = 0
@@ -8,6 +19,9 @@ file.close()
 
 maxArea = 0
 
+# for i in range(len(data)):
+#     line = data[i]
+
 for i in range(len(data)):
     line = data[i]
     
@@ -15,7 +29,10 @@ for i in range(len(data)):
     x, y = int(x), int(y)
     
     for n in range(i + 1, len(data)):
+        isValid = False
         nLine = data[n]
+        if not isValid:
+            continue
         nX, nY = nLine.strip().split(",")
         nX, nY = int(nX), int(nY)
         
@@ -23,5 +40,5 @@ for i in range(len(data)):
         
         if area > maxArea:
             maxArea = area
-        
+
 print(maxArea)
